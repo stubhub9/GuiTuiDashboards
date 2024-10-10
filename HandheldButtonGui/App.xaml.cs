b909e77ef592567e4e291bdc0ab54873a45431eb
bumble <bumble@DUSTY>
2024-10-10 16:21:58 -0700

    Add project files.

---
+++ HandheldButtonGui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HandheldButtonGui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ HandheldButtonGui/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandheldButtonGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region  Fields and Properties
+        private Brush _bannerBrush1 = Brushes.Cyan;
+        public Brush BannerBrush1
+        {
+            get
+            {
+                return _bannerBrush1;
+            }
+            set
+            {
+                _bannerBrush1 = value;
+                INotifyPropertyChanged ( () => BannerBrush1 );
+            }
+        }
+
+        
+
+
+
+        #endregion  end of Fields and Properties
+
+
+
+        #region  Constructor
+        public MainWindow ()
+        {
+            InitializeComponent ();
+        }
+
+        #endregion  end of Constructor
+
+
+
+
+
+        #region  Bindings
+
+        //public class ColorToBrushConverter1 : IValueConverter
+        //{
+        //    public object Convert ( object value, Type targetType, object parameter, CultureInfo culture )
+        //    {
+        //        return new SolidColorBrush((Color)value);
+        //    }
+
+        //    public object ConvertBack ( object value, Type targetType, object parameter, CultureInfo culture )
+        //    {
+        //        //return (value as SolidColorBrush).Color;
+        //        return SolidColorBrush.Red;
+        //    }
+        //}
+
+        private void INotifyPropertyChanged ( Func<Brush> value )
+        {
+            throw new NotImplementedException ();
+        }
+
+
+        #endregion  end Bindings
+    }
+}
