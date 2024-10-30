166395d16b63d48c87b53e87fedc773a122e700d
Stu Bilyeu <stublue1@outlook.com>
2024-10-29 20:57:22 -0700

    Combined switch and finger to button. Resized window.

--- HandheldButtonGui/MainWindow.xaml.cs
+++ HandheldButtonGui/MainWindow.xaml.cs
@@ -22,6 +22,12 @@ namespace HandheldButtonGui
 
         #region  Fields and Properties
 
+        //  Project Flag
+        private bool _IsEnergized = false;
+        private Brush brush_LampOn = Brushes.Yellow;
+
+
+        //TODO:  Banner, Color schemes, and what belongs in properties?  
         private Brush _bannerBrush1 = Brushes.Cyan;
         public Brush BannerBrush1
         {
@@ -36,7 +42,7 @@ namespace HandheldButtonGui
             }
         }
 
-        
+
 
 
 
@@ -56,7 +62,7 @@ namespace HandheldButtonGui
 
 
 
-        #region  Bindings
+        #region  Bindings and Events
 
         //public class ColorToBrushConverter1 : IValueConverter
         //{
@@ -78,6 +84,88 @@ namespace HandheldButtonGui
         }
 
 
-        #endregion  end Bindings
+
+        private void fingerButton_Click ( object sender, RoutedEventArgs e )
+        {
+            /* 
+             * Begin animation dropping the finger and switch to the lower position.
+             * 
+             *      
+             *  The switch leg will be ENERGIZED anytime the switch is closed; 
+             *      FROM the END of the Switch Closing animation,
+             *      UNTIL the START of the Switch Opening animation.
+             *  
+             *  When ENERGIZED, the Motor will spin and the Light will shine.
+             */
+
+
+            //  Toggle the finger final position; using animation.
+
+
+            //var button1 = dpstButton;
+
+            if (!_IsEnergized)
+            {
+                CloseSwitch ();
+                //  Complete the Closing the Switch animation.
+
+                //  THEN
+                //  Start spinning the motor.
+                //  Change Lamp to Light color.
+            }
+            else
+            {
+                //  Begin Animate fingerButton and toggle switch to open position.
+                //  AND
+                //  Stop spinning the motor.
+                //  Change Lamp to Dark color.
+            }
+
+
+        }
+
+
+
+
+
+
+
+        #endregion   Bindings and Events
+
+
+
+        #region     Methods
+
+
+        private void CloseSwitch ()
+        {
+            throw new NotImplementedException ();
+        }
+
+
+
+        private void ToggleEnergized ()
+        {
+            //  ToggleEnergized should occur when:
+            // the switch enters or leaves the closed position.
+            ToggleEnergized_Light ();
+            ToggleEnergized_Motor ();
+
+        }
+
+        private void ToggleEnergized_Motor ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        private void ToggleEnergized_Light ()
+        {
+            throw new NotImplementedException ();
+        }
+
+
+
+        #endregion  Methods
+
     }
 }
